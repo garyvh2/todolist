67a78a59e8a7d88124ad75dc6b3ae2f788658f98
Gary Valverde Hampton <garyhampton.12@gmail.com>
2019-02-22 21:08:42 -0600

    feat(components): add image placeholder

--- packages/todolist-client/src/app/app.module.ts
+++ packages/todolist-client/src/app/app.module.ts
@@ -1,21 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { TodoService } from './services/todo.service';
-import { HttpClientModule } from '@angular/common/http';
-import { TodoCardComponent } from './shared/components/todo-card/todo-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatButtonModule, MatInputModule } from '@angular/material';
-import { TodoListComponent } from './shared/components/todo-list/todo-list.component';
-import { TodoAddComponent } from './shared/components/todo-add/todo-add.component';
-
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./views/dashboard/dashboard.component";
+import { TodoService } from "./services/todo.service";
+import { HttpClientModule } from "@angular/common/http";
+import { TodoCardComponent } from "./shared/components/todo-card/todo-card.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule
+} from "@angular/material";
+import { TodoListComponent } from "./shared/components/todo-list/todo-list.component";
+import { TodoAddComponent } from "./shared/components/todo-add/todo-add.component";
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, TodoCardComponent, TodoListComponent, TodoAddComponent],
-    imports: [HttpClientModule, FormsModule, BrowserModule, BrowserAnimationsModule, MatCardModule, MatButtonModule, MatInputModule],
-    providers: [TodoService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    TodoCardComponent,
+    TodoListComponent,
+    TodoAddComponent
+  ],
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
